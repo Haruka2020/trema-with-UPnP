aa624576379844f6fe0411820639dac635f033c8
Haruka2020 <vessel.bum1077@gmail.com>
2017-07-09 18:27:17 +0900

    ruby mysql test

--- OF_controller/mysql_test.rb
+++ OF_controller/mysql_test.rb
@@ -1,7 +1,7 @@
 require 'mysql2'
 
 client = Mysql2::Client.new(:host => 'localhost', :user => 'root', :password => 'password')
-query = %q{select deviceType from UPnP_List.rootdevice}
+query = %q{select deviceType from UPnP_List.rootDevice}
 results = client.query(query)
 results.each do |row|
   puts "--------------------"
