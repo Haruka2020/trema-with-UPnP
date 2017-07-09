9a1c3519a8f5ff0a216e40958b89713fa45fccb3
Haruka2020 <vessel.bum1077@gmail.com>
2017-07-09 19:38:24 +0900

    ruby mysql test

--- OF_controller/mysql_test.rb
+++ OF_controller/mysql_test.rb
@@ -1,7 +1,10 @@
 require 'mysql2'
+ip = '192.168.11.1'
+port = '50324'
 
 client = Mysql2::Client.new(:host => 'localhost', :user => 'root', :password => 'password')
-query = %q{select deviceType from UPnP_List.rootDevice}
+query = %Q{select * from UPnP_List.rootDevice Where ip = '#{ip}' and port = '#{port}'}
+#query = %q{select * from UPnP_List.rootDevice Where port = '50324' and ip = '192.168.11.1'}
 results = client.query(query)
 results.each do |row|
   puts "--------------------"
