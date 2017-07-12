2596eb329221e129130edfb56997c425ab6751fc
Haruka2020 <vessel.bum1077@gmail.com>
2017-07-12 14:25:16 +0900

    admission add htmlfolder

--- Web_Push_module/html/admission.php
+++ Web_Push_module/html/admission.php
@@ -1,15 +1,13 @@
 <?php
 session_start();
   if(isset($_SESSION['id'])&&isset($_SESSION['password'])){
-    $socket = socket_create(AF_INET,SOCK_STREAM,SOL_TCP);
-    socket_connect($socket,'127.0.0.1',9999);
-    $id = $_SESSION['id'];
-    $password = $_SESSION['password'];
-    $admission = $_SESSION['admission'];
-    $dpid = $_SESSION['dpid'];
-    socket_write($socket,"$id,$password,$admission,$dpid");
-    socket_close($socket);
-    echo "ok";
+    $id = escapeshellcmd($_SESSION['id']);
+    $password = escapeshellcmd($_SESSION['password']);
+    $admission = escapeshellcmd($_SESSION['admission']);
+    $dpid = escapeshellcmd($_SESSION['dpid']);
+
+    system("ruby admission-test.rb $dpid $password $admission");
+
   }else {
     echo "エラー：リンクからアクセスして下さい";
   }
