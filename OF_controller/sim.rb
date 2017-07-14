c6488ce55f95236245173752f00d169b2f3ad6e4
Haruka2020 <vessel.bum1077@gmail.com>
2017-07-14 21:02:20 +0900

    test refine

--- OF_controller/sim.rb
+++ OF_controller/sim.rb
@@ -23,15 +23,17 @@ class Sim < Trema::Controller
     end
   end
   def packet_in_ipv4(dpid,packet_in)
-  	puts "ipv4 destination"
-		puts packet_in.destination_ip_address.to_s
+  	puts "ipv4 destination #{packet_in.destination_ip_address.to_s}"
+		
 		if(packet_in.destination_ip_address.to_s == '192.168.11.11')
 			puts "test start"
 			begin
 				d_ip,d_port,s_ip = packet_in.destination_ip_address,packet_in.transport_destination_port,packet_in.source_ip_address
-				body,password,remote_password,id = `ruby lan_suggest.rb #{d_ip} #{d_port} #{s_ip} #{dpid}`
+				body,password,remote_password,id = `ruby lan_suggest.rb 192.168.11.9 49152 #{s_ip} #{dpid}`
 				system("ruby send_mail/send_message.rb '#{body}'")
+				puts body
 				@packet_stock[dpid][id] = packet_in
+				stop_packet(packet_in)
 			rescue => e
 				puts e
 				binding.pry
@@ -57,6 +59,16 @@ class Sim < Trema::Controller
      	actions: SendOutPort.new(out_port)
     )	
     packet_out(packet_in,out_port)
+  end
+  def stop_packet(packet_in)
+    send_flow_mod_add(
+      packet_in.datapath_id,
+      match: Match.new(
+      	source_mac_address: packet_in.source_mac,
+      	ether_type: packet_in.ether_type,
+      	priority: 1
+      )
+    )	
   end
 	def packet_out(packet_in, port_no)
     send_packet_out(
